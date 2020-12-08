6b79ec123cea0e47acd8d0bb1b9df177162fa1e0
meerakodiyatar <meerakodiyatar@gmail.com>
2020-12-08 15:26:25 +0530

    nav-fixed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,17 +18,17 @@
       }
     }
   });
-
+  $("header").addClass("background-header");
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     var box = $(".header-text").height();
     var header = $("header").height();
 
-    if (scroll >= box - header) {
-      $("header").addClass("background-header");
-    } else {
-      $("header").removeClass("background-header");
-    }
+    // if (scroll >= box - header) {
+    //   $("header").addClass("background-header");
+    // } else {
+    //   $("header").removeClass("background-header");
+    // }
   });
 
   // Mobile menu dropdown
